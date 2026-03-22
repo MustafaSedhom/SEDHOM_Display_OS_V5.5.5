c362ec0c4954f1f67ba4a00452ee40abf97ab6ca
Eng.Mostafa Sedhom <elmohandes24680@gmail.com>
2026-03-22 05:52:52 +0200

    add new file and add on it class for make new dqatat type in my lib to can change datatype of variable at run time and value of it

---
+++ SEDHOM_Display_OS/src/SEDHOM_Dynamic_Data_Type.h
@@ -0,0 +1,25 @@
+#ifndef SEDHOM_DYNAMIC_DATA_TYPE_H_
+#define SEDHOM_DYNAMIC_DATA_TYPE_H_
+//:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+
+//:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+class SEDHOM_Dynamic_Data_Type
+{
+private:
+    /* data */
+public:
+    SEDHOM_Dynamic_Data_Type();
+    ~SEDHOM_Dynamic_Data_Type();
+};
+//:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+// define all variables
+SEDHOM_Dynamic_Data_Type::SEDHOM_Dynamic_Data_Type()
+{
+}
+
+SEDHOM_Dynamic_Data_Type::~SEDHOM_Dynamic_Data_Type()
+{
+}
+
+//:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+#endif // !SEDHOM_DYNAMIC_DATA_TYPE_H_
