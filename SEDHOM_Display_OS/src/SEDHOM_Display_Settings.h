8e093acc618960925f93e35d63b614798c2f8740
Eng.Mostafa Sedhom <elmohandes24680@gmail.com>
2026-08-24 02:05:18 +0300

    update setting file

--- SEDHOM_Display_OS/src/SEDHOM_Display_Settings.h
+++ SEDHOM_Display_OS/src/SEDHOM_Display_Settings.h
@@ -13,20 +13,20 @@
 #define API_MCU_Pin_Direction(__pin__,__mode__)                pinMode(__pin__, __mode__)      
 #define API_MCU_Pin_Read(__pin__)                              digitalRead(__pin__)      
 #define API_MCU_Pin_Write(__pin__,__mode__)                    digitalWrite(__pin__, __mode__)      
-#define API_MCU_Pin_Mode_INPUT                                0x0      
+#define API_MCU_Pin_Mode_INPUT                                 0x0      
 #define API_MCU_Pin_Mode_OUTPUT                                0x1      
-#define API_MCU_Pin_Mode_INPUT_PULLUP                         0x2      
-#define API_MCU_Pin_Mode_INPUT_PULLDOWN                       0x2      
+#define API_MCU_Pin_Mode_INPUT_PULLUP                          0x2      
+#define API_MCU_Pin_Mode_INPUT_PULLDOWN                        0x2      
 //============================================================================================================================================
 // Display Settings
 //============================================================================================================================================
 // include Driver Lib
 #include <MCUFRIEND_kbv.h>
 // define APIs from driver
-#define API_Driver_Class_Name                                   MCUFRIEND_kbv      
-#define API_Display_Object_Name                                 SEDHOM_Display  
+#define API_Driver_Class_Name                                    MCUFRIEND_kbv      
+#define API_Display_Object_Name                                  SEDHOM_Display  
 #define API_Driver_Parameters                                   
-#define API_Make_New_Display                                    API_Driver_Class_Name  API_Display_Object_Name   API_Driver_Parameters
+#define API_Make_New_Display                                     API_Driver_Class_Name  API_Display_Object_Name   API_Driver_Parameters
 // Make Object of Display Driver to Pass APIs
 API_Make_New_Display;
 //>>>>> APIs
@@ -52,15 +52,15 @@ API_Make_New_Display;
 // include Driver Lib
 #include <TouchScreen.h>
 // Pins
-#define YP A3  // must be an analog pin, use "An" notation!
-#define XM A2  // must be an analog pin, use "An" notation!
-#define YM 9   // can be a digital pin
-#define XP 8   // can be a digital pin
+#define Touch_Y_Positive_Pin A3  // Y+ : reads Y coordinate (analog)
+#define Touch_X_Negative_Pin A2  // X- : reads X coordinate (analog)
+#define Touch_Y_Negative_Pin 9   // Y- : drive pin (digital)
+#define Touch_X_Positive_Pin 8   // X+ : drive pin (digital)  
 // define APIs from driver
 #define API_Touch_Driver_Class_Name                      TouchScreen
-#define API_Touch_Point_Driver_Class_Name                 TSPoint
+#define API_Touch_Point_Driver_Class_Name                TSPoint
 #define API_Touch_Object_Name                            SEDHOM_Touch_Driver
-#define API_Touch_Driver_Parameters                      (XP, YP, XM, YM, 300)
+#define API_Touch_Driver_Parameters                      (Touch_X_Positive_Pin, Touch_Y_Positive_Pin, Touch_X_Negative_Pin, Touch_Y_Negative_Pin, 300)
 #define API_Make_New_Touch                               API_Touch_Driver_Class_Name  API_Touch_Object_Name  API_Touch_Driver_Parameters
 // Make Object of Touch Driver to Pass APIs
 API_Make_New_Touch;
@@ -68,12 +68,12 @@ API_Make_New_Touch;
 #define API_Get_Touch_Point()                             API_Touch_Object_Name.getPoint()
 #define API_Set_Touch_Pins_Setting()                      API_MCU_Pin_Direction(YP, API_MCU_Pin_Mode_OUTPUT);API_MCU_Pin_Direction(XM, API_MCU_Pin_Mode_OUTPUT);API_MCU_Pin_Write(YP, 1);API_MCU_Pin_Write(XM, 1);
 // Calibration
-#define API_Touch_X_Raw_Left_Edge()                        954
-#define API_Touch_X_Raw_Right_Edge()                       88
-#define API_Touch_Y_Raw_Top_Edge()                         908
-#define API_Touch_Y_Raw_Bottom_Edge()                      125
-#define API_Touch_Pressure_Min()                           10
-#define API_Touch_Pressure_Max()                           1000
+#define API_Touch_X_Raw_Left_Edge()                       954
+#define API_Touch_X_Raw_Right_Edge()                      88
+#define API_Touch_Y_Raw_Top_Edge()                        908
+#define API_Touch_Y_Raw_Bottom_Edge()                     125
+#define API_Touch_Pressure_Min()                          10
+#define API_Touch_Pressure_Max()                          1000
 //============================================================================================================================================
 
 
